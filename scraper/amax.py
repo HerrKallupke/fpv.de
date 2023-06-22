4200a1ec39e78060adce7e2191ef796a89a16d08
HerrKallupke <77732408+HerrKallupke@users.noreply.github.com>
2023-06-22 16:20:57 +0200

    added pycharm

--- scraper/amax.py
+++ scraper/amax.py
@@ -20,16 +20,18 @@ def scrape():
 
     print('Crawling pages...')
     for i in range(pages):
-        product_page = requests.get(URL + i)
+        product_page = requests.get(URL + str(i))
         product_soup = BeautifulSoup(product_page.content, 'html.parser')
 
         products_raw = product_soup.select('.str-items-grid__container')[0].find_all('article')
 
-        for product in products_raw:
-            url = product.select('div:nth-child(3) > h3:nth-child(1) > a:nth-child(1)')[0].get('href')
-            title = product.select('div:nth-child(3) > h3:nth-child(1) > a:nth-child(1) > span:nth-child(2)')[0].text
-            image = product.select('a:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > picture:nth-child(1) > img:nth-child(2)')[0]['src']
-            price = float(product.select('div:nth-child(3) > span:nth-child(2)')[0].text.replace(',', '.').replace('€', '').replace('EUR', '').replace(' ', ''))
+        print(len(products_raw))
+
+        for product in range(len(products_raw)):
+            url = product_soup.select(f'article.str-item-card:nth-child({product + 1}) > a:nth-child(2) > div:nth-child(1) > div:nth-child(1) > h3:nth-child(2) > a:nth-child(1)')[0].get('href')
+            title = product_soup.select(f'article.str-item-card:nth-child({product + 1}) > a:nth-child(2) > div:nth-child(1) > div:nth-child(1) > h3:nth-child(2) > a:nth-child(1) > span:nth-child(2)')[0].text
+            image = product_soup.select(f'article.str-item-card:nth-child({product + 1}) > a:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > picture:nth-child(1) > img:nth-child(2)')[0]['src']
+            price = float(product_soup.select(f'article.str-item-card:nth-child({product + 1}) > div:nth-child(3) > span:nth-child(1)')[0].text.replace(',', '.').replace('€', '').replace('EUR', '').replace(' ', ''))
 
             products.append((url, title, image, price))
 
